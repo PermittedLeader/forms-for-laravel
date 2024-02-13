8a585c66791d084a726cf20adaa2e428b070b3cc
Myles Smith <hello@itsmyl.es>
2024-02-13 14:16:05 +0000

    changing alert component render

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{open: true}" class="border-solid rounded-sm {{ $classes }}" x-show="open" x-transition:leave="transition ease-in-out duration-300" x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-0" role="alert">
+<div x-data="{open: true}" class="border-solid rounded-sm {{ $componentData->classes }}" x-show="open" x-transition:leave="transition ease-in-out duration-300" x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-0" role="alert">
     <div class="flex flex-row">
         <div class="flex-grow">
             <div class="flex flex-col h-full">
@@ -23,10 +23,10 @@
                 <div class="my-auto text-right">
                     <x-icon icon="{{ $icon }}" label="{{ $level }}" />
                 </div>
-                @if($dismissable)
+                @if($componentData->dismissable)
                 <div class="ml-3" x-on:click='open = !open' role="button">
                     <span class="cursor-pointer">
-                        <svg class="h-3 w-3 rounded-sm {{ $buttonStyle }} hover:bg-opacity-75 hover:shadow-inner" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <svg class="h-3 w-3 rounded-sm {{ $componentData->buttonStyle }} hover:bg-opacity-75 hover:shadow-inner" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                         <span class="sr-only">{{ __('Close') }}</span>
