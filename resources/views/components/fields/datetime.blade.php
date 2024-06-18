bfe4e890c95f339ef76c30b7f207f983b6551c04
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-06-18 20:07:02 +0100

    set type of datetime field

--- resources/views/components/fields/datetime.blade.php
+++ resources/views/components/fields/datetime.blade.php
@@ -1,5 +1,10 @@
 @if($method != "show")
-    <x-tiffey::input.date-time label="{{ $fieldObject->getLabel() }}" name="{{ $fieldObject->key }}" required="{{ $fieldObject->required }}" />
+    <x-forms::input.date-time 
+        label="{{ $fieldObject->getLabel() }}" 
+        name="{{ $fieldObject->key }}" 
+        required="{{ $fieldObject->required }}" 
+        type="{{ $fieldObject->type }}"
+        />
 @else
     <x-forms::show.text
         label="{{ $fieldObject->getLabel() }}" 
--- src/View/Components/Fields/Datetime.php
+++ src/View/Components/Fields/Datetime.php
@@ -5,4 +5,21 @@ namespace Permittedleader\Forms\View\Components\Fields;
 class Datetime extends Field
 {
     public string $component = 'fields.datetime'; 
+
+    public string $type = "datetime";
+
+    /**
+     * Set the type of the field
+     * 
+     * Datetime, date, time are valid options
+     *
+     * @param string $type
+     * @return self
+     */
+    public function type($type = "datetime"): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
 }
