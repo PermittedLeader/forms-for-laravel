c63ab986680b756b605386e83880564a68e034aa
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-06-23 17:07:41 +0100

    creating error partial

--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -16,18 +16,7 @@
                 @lang('Show') {{ $this->getLabel() }}
             @endif
         </x-slot>
-        @if($errors->any())
-            <x-tiffey::alert level="warning">
-                <x-slot name="header">
-                    {{ __('You have errors in your form') }}
-                </x-slot>
-                <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-                </ul>
-            </x-tiffey::alert>
-        @endif
+        <x-forms::partials.errors />
         @if($this->method == "edit")
             @foreach ($this->fieldsForEdit() as $field)
                 {{ $field->renderForEditing() }}
