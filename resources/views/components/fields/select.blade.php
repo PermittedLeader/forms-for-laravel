c0b2f53c9fdaf92992ca3ab47d8e617d19a7dbcb
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-11-01 12:44:14 +0000

    Update select.blade.php

--- resources/views/components/fields/select.blade.php
+++ resources/views/components/fields/select.blade.php
@@ -1,6 +1,7 @@
 @if($method != "show")
     <x-forms::input.select label="{{ $fieldObject->getLabel() }}" name="{{ $fieldObject->key }}" required="{{ $fieldObject->required }}">
-        @foreach ($fieldObject->options as $option)
+        @foreach ($fieldObject->options as $key => $option)
+            @if(is_object($option))
             <option 
                 value="{{ $option->{$fieldObject->keyAttribute} }}" 
                 @if($method == "edit")
@@ -8,10 +9,22 @@
                         old($fieldObject->key,$fieldObject->inputValue()) == $option->{$fieldObject->keyAttribute}
                     )
                 @endif
-                
-                >
+            >
                 {{ $option->{$fieldObject->displayAttribute} }}
             </option>
+            @else
+            <option 
+                value="{{ $key }}" 
+                @if($method == "edit")
+                    @selected(
+                        old($fieldObject->key,$fieldObject->inputValue()) == $option
+                    )
+                @endif
+            >
+                {{ $option }}
+            </option>
+            @endif
+            
         @endforeach 
     </x-forms::input.select>
 @else
